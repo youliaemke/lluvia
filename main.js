abc7ea6088d8a91e2d39f76a72c4a348d11378dc
pascalmorgenthaler <youlia.emke@gmail.com>
2023-10-17 10:48:35 +0200

    a

--- main.js
+++ main.js
@@ -7,20 +7,30 @@ $( document ).ready(function() {
 
 
 	$('.slider-container').slick({
-		autoplay: true, // Do we want it to autoplay? true or false
-		autoplaySpeed: 3000, // How long between each slide when auto-playing
-		speed: 200, // How fast is the transition in milliseconds
-		arrows: true, // Do you want to show arrows to trigger each slide
-		accessibility: true, // Enables keyboard tabbing and arrow key navigation
-		dots: false, // Enables the dots below to show how many slides
-		fade: false, // Changes the animate from slide to fade if true
-		infinite: true, // When true, means that it will scroll in a circle
-		pauseOnHover: false, // When true means the autoplay pauses when hovering
-		pauseOnDotsHover: false, // Pauses the autoplay when hovering over the dots
+		autoplay: true,        // Do we want it to autoplay? true or false
+		autoplaySpeed: 10000,   // How long between each slide when autoplaying
+		speed: 500,            // How fast is the transition 
+		arrows: true,          // Do you want to show arrows to trigger each slide
+		accessibility: true,   // Enables tabbing and arrow key navigation 
+		dots: true,            // Enables the dots below to show how many slides
+		fade: false,           // Changes the animate from slide to fade if true
+		infinite: true,       // When true, means that it will scroll in a circle
+		pauseOnHover: true,   // When true means the autoplay pauses when hovering
+		pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
 		slidesToShow: 1,
-		slidedToScroll: 1,
-		fade: true
-	}); 
+		slidesToScroll: 1,
+		responsive: [
+		  {
+			breakpoint: 768,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1
+			}
+		  }
+		]
+	  });
+
+
 	  
 
 });
