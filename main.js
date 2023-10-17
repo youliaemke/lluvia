81d4e36876373c0945165c51181144ed7ccc4b34
pascalmorgenthaler <youlia.emke@gmail.com>
2023-10-17 14:08:40 +0200

    a

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ $( document ).ready(function() {
 
 
 	$('.slider-container').slick({
-		autoplay: false,        // Do we want it to autoplay? true or false
-		autoplaySpeed: 10000,   // How long between each slide when autoplaying
+		autoplay: true,        // Do we want it to autoplay? true or false
+		autoplaySpeed: 9000,   // How long between each slide when autoplaying
 		speed: 500,            // How fast is the transition 
 		arrows: true,          // Do you want to show arrows to trigger each slide
 		accessibility: true,   // Enables tabbing and arrow key navigation 
 		dots: true,            // Enables the dots below to show how many slides
-		fade: false,           // Changes the animate from slide to fade if true
+		fade: true,           // Changes the animate from slide to fade if true
 		infinite: true,       // When true, means that it will scroll in a circle
 		pauseOnHover: true,   // When true means the autoplay pauses when hovering
 		pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
